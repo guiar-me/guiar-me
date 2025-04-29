f6b33ae6325c2da9a795bc05805ed1296ba2e5d5
Marques Yuri de Souza <souz4.my@gmail.com>
2025-04-29 15:55:30 -0300

    refactor: remove unused extensions

--- packages/sdks/flutter/lib/core/extension/text_input_formatter_extension.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/services.dart';
-
-extension TextInputFormatterExtension on TextEditingValue {
-  String onlyNumbers() {
-    String numbers = text.replaceAll(RegExp(r'[^\d]'), '');
-
-    return numbers;
-  }
-}
